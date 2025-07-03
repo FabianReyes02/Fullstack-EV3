8a3a66a384b7338a70f7fbb7e2738b2ef3d7dd6f
Brian <163011832+brx2b@users.noreply.github.com>
2025-07-03 18:02:15 -0400

    Delete pedidoservice directory

--- pedidoservice/src/main/java/com/perfulandia/pedidoservice/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.perfulandia.pedidoservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.perfulandia.pedidoservice.model.Pedido;
-import com.perfulandia.pedidoservice.repository.PedidoRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-}
